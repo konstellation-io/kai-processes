d39b5c7f93eb4233c9164adea2d08ff57efb3380
Alex Rodriguez <alex.rodriguez@intelygenz.com>
2023-07-20 18:16:19 +0200

    no more lint in errors

--- github-webhook-trigger/internal/webhook/errors.go
+++ github-webhook-trigger/internal/webhook/errors.go
@@ -5,26 +5,28 @@ import (
 	"fmt"
 )
 
-var formatTwoErrors = "%w: %w" //nolint:gochecknoglobals
-
 var ErrEventNotSupported = errors.New("event not supported")
 var ErrNotAValidEvent = errors.New("not a valid event")
 var ErrGettingEventsFromConfig = errors.New("error getting events from config")
 var ErrCreatingWebhook = errors.New("error creating webhook")
 var ErrServer = errors.New("error listening and serving")
 
+func concateErrors(err1, err2 error) error {
+	return fmt.Errorf("%w: %w", err1, err2)
+}
+
 func NotValidEventError(event string) error {
 	return fmt.Errorf("%q is %w", event, ErrNotAValidEvent)
 }
 
 func GettingEventsFromConfigError(err error) error {
-	return fmt.Errorf(formatTwoErrors, ErrGettingEventsFromConfig, err) //nolint:goerr113
+	return concateErrors(ErrGettingEventsFromConfig, err)
 }
 
 func CreatingWebhookError(err error) error {
-	return fmt.Errorf(formatTwoErrors, ErrCreatingWebhook, err) //nolint:goerr113
+	return concateErrors(ErrCreatingWebhook, err)
 }
 
 func ServerError(err error) error {
-	return fmt.Errorf(formatTwoErrors, ErrServer, err) //nolint:goerr113
+	return concateErrors(ErrServer, err)
 }

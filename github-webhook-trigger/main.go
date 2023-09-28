e5d2dcdea05a612429a659659b7ebfb12e162f94
Alex Rodriguez <alex.rodriguez@intelygenz.com>
2023-09-28 17:30:42 +0200

    move server init to initializer

--- github-webhook-trigger/main.go
+++ github-webhook-trigger/main.go
@@ -7,20 +7,30 @@ import (
 	"github.com/konstellation-io/kai-sdk/go-sdk/runner"
 	"github.com/konstellation-io/kai-sdk/go-sdk/runner/trigger"
 	"github.com/konstellation-io/kai-sdk/go-sdk/sdk"
+	"google.golang.org/protobuf/types/known/anypb"
 )
 
-func runnerFunc(wh webhook.Webhook) trigger.RunnerFunc {
-	return func(tr *trigger.Runner, sdk sdk.KaiSDK) {
-		err := wh.InitWebhook(sdk)
-		if err != nil {
-			sdk.Logger.Error(err, "error creating webhook")
-			os.Exit(1)
-		}
+func initializer(sdk sdk.KaiSDK) {
+	sdk.Logger.Info("Initializing webhook")
+
+	wh := webhook.NewGithubWebhook()
+
+	err := wh.InitWebhook(sdk)
+	if err != nil {
+		sdk.Logger.Error(err, "error creating webhook")
+		os.Exit(1)
 	}
 }
 
+func runnerFunc(tr *trigger.Runner, sdk sdk.KaiSDK) {
+	sdk.Messaging.SendOutput(&anypb.Any{})
+}
+
 func main() {
-	wh := webhook.NewGithubWebhook()
-	r := runner.NewRunner().TriggerRunner().WithRunner(runnerFunc(wh))
+	r := runner.NewRunner().
+		TriggerRunner().
+		WithInitializer(initializer).
+		WithRunner(runnerFunc)
+
 	r.Run()
 }

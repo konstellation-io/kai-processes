d1110d4b7ed884e32baf06f7a34abba8e5415804
Alex Rodriguez Fernandez <alex.rodriguez@intelygenz.com>
2023-09-29 12:57:08 +0200

    fix(github-webhook): move server init to initializer (#11)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    <!--- Describe your changes in detail -->
    - Fix webhook creation
    
    ## Motivation and Context
    <!--- Why is this change required? What problem does it solve? -->
    <!--- If it fixes an open issue, please link to the issue here. -->
    This is the correct use to use a kai trigger
    
    ## Types of changes
    <!--- What types of changes does your code introduce? Put an `x` in all the boxes that apply: -->
    - [X] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    - [ ] Other changes (ci configuration, documentation or any other kind of changes)
    
    ## Checklist:
    <!--- Go over all the following points, and put an `x` in all the boxes that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're here to help! -->
    - [ ] I have created tests for my code changes, and the tests are passing.
    - [ ] I have executed the pre-commit hooks locally.
    - [ ] My change requires a change to the documentation (create a new issue if the documentation has not been updated).
    - [ ] I have updated the documentation accordingly.

--- github-webhook-trigger/main.go
+++ github-webhook-trigger/main.go
@@ -9,18 +9,29 @@ import (
 	"github.com/konstellation-io/kai-sdk/go-sdk/sdk"
 )
 
-func runnerFunc(wh webhook.Webhook) trigger.RunnerFunc {
-	return func(tr *trigger.Runner, sdk sdk.KaiSDK) {
-		err := wh.InitWebhook(sdk)
+func initializer(wh webhook.Webhook) func(kaiSDK sdk.KaiSDK) {
+	return func(kaiSDK sdk.KaiSDK) {
+		kaiSDK.Logger.Info("Initializing webhook")
+
+		err := wh.InitWebhook(kaiSDK)
 		if err != nil {
-			sdk.Logger.Error(err, "error creating webhook")
+			kaiSDK.Logger.Error(err, "error creating webhook")
 			os.Exit(1)
 		}
 	}
 }
 
+func runnerFunc(tr *trigger.Runner, kaiSDK sdk.KaiSDK) {
+	kaiSDK.Logger.Info("Running webhook handler")
+}
+
 func main() {
 	wh := webhook.NewGithubWebhook()
-	r := runner.NewRunner().TriggerRunner().WithRunner(runnerFunc(wh))
+
+	r := runner.NewRunner().
+		TriggerRunner().
+		WithInitializer(initializer(wh)).
+		WithRunner(runnerFunc)
+
 	r.Run()
 }

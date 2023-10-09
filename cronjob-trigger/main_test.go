1af3e4cd7180d1a8d612489e5dba3a3108c56979
Javier Aguilera <jaguilerapuerta@gmail.com>
2023-10-09 16:33:00 +0200

    feat: initial test

--- cronjob-trigger/main_test.go
+++ cronjob-trigger/main_test.go
@@ -3,19 +3,21 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"testing"
 
+	"github.com/go-logr/logr/testr"
+	sdkMocks "github.com/konstellation-io/kai-sdk/go-sdk/mocks"
 	"github.com/konstellation-io/kai-sdk/go-sdk/sdk"
+	"github.com/konstellation-io/kai-sdk/go-sdk/sdk/messaging"
 	"github.com/stretchr/testify/suite"
 )
 
 type MainSuite struct {
 	suite.Suite
 
-	kaiSdkMock        sdk.KaiSDK
-	cronjobMock *mocks.CronjobMock
+	centralizedConfigMock *sdkMocks.CentralizedConfigMock
+	messagingMock         *sdkMocks.MessagingMock
+	kaiSdk                sdk.KaiSDK
 }
 
 func TestMainSuite(t *testing.T) {
@@ -23,35 +25,31 @@ func TestMainSuite(t *testing.T) {
 }
 
 func (s *MainSuite) SetupSuite() {
-	s.cronjobMock = mocks.NewCronjobMock(s.T())
-	s.kaiSdkMock = sdk.KaiSDK{}
+	s.messagingMock = sdkMocks.NewMessagingMock(s.T())
+	s.centralizedConfigMock = sdkMocks.NewCentralizedConfigMock(s.T())
+
+	s.kaiSdk = sdk.KaiSDK{
+		Logger:            testr.New(s.T()),
+		Messaging:         s.messagingMock,
+		CentralizedConfig: s.centralizedConfigMock,
+	}
 }
 
 func (s *MainSuite) TearDownTest() {
-	s.cronjobMock.AssertExpectations(s.T())
-	s.cronjobMock.ExpectedCalls = nil
+	s.messagingMock.AssertExpectations(s.T())
+	s.centralizedConfigMock.AssertExpectations(s.T())
+	s.messagingMock.ExpectedCalls = nil
+	s.centralizedConfigMock.ExpectedCalls = nil
+	s.messagingMock.Calls = nil
 }
 
 func (s *MainSuite) TestInitializer() {
-	initializer(s.cronjobMock)
+	initializer(s.kaiSdk)
 }
 
-func (s *MainSuite) TestRunnerFunc() {
-	s.cronjobMock.On("CronjobMock", s.cronjobMock).Return(nil)
+func (s *MainSuite) TestCronjobRunnerFunc() {
+	s.centralizedConfigMock.On("GetConfig", "cron", messaging.ProcessScope).Return("30 * * * * *", nil)
+	s.centralizedConfigMock.On("GetConfig", "message", messaging.ProcessScope).Return("test message", nil)
 
-	runnerFunc(s.cronjobMock)(nil, s.cronjobMock)
-}
-
-func (s *MainSuite) TestRunnerFuncError() {
-	s.cronjobMock.On("CronjobMock", s.cronjobMock).Return(fmt.Errorf("mocked error"))
-
-	fakeExitCalled := 0
-	fakeExit := func(int) {
-		fakeExitCalled++
-	}
-
-	patch := monkey.Patch(os.Exit, fakeExit)
-	defer patch.Unpatch()
-
-	runnerFunc(s.cronjobMock)(nil, s.cronjobMock)
+	cronjobRunner(nil, s.kaiSdk)
 }

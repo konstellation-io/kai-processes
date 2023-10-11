e96574354cebf0a662512db571fd8322abfddc36
Alex Rodriguez Fernandez <alex.rodriguez@intelygenz.com>
2023-10-11 10:38:44 +0200

    Update gitlab-webhook-trigger/minikube_build.sh
    Co-authored-by: David Fuentes <51330112+dfinteligenz@users.noreply.github.com>

--- gitlab-webhook-trigger/minikube_build.sh
+++ gitlab-webhook-trigger/minikube_build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 CGO_ENABLED=0 go build -o build/process main.go
-minikube image build -t kai-processes/github-webhook-trigger . -f develop.Dockerfile -p kai-local
+minikube image build -t kai-processes/gitlab-webhook-trigger . -f develop.Dockerfile -p kai-local

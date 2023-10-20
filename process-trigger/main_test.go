b875c41cefa10f2a1c929b2a7bdcb470204abf9d
Javier Aguilera <jaguilerapuerta@gmail.com>
2023-10-20 17:42:35 +0200

    feat: update tests

--- process-trigger/main_test.go
+++ process-trigger/main_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/go-logr/logr/testr"
 	sdkMocks "github.com/konstellation-io/kai-sdk/go-sdk/mocks"
 	"github.com/konstellation-io/kai-sdk/go-sdk/sdk"
-	"github.com/konstellation-io/kai-sdk/go-sdk/sdk/messaging"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 )
@@ -17,6 +16,7 @@ type MainSuite struct {
 	suite.Suite
 
 	centralizedConfigMock *sdkMocks.CentralizedConfigMock
+	metadataMock          *sdkMocks.MetadataMock
 	messagingMock         *sdkMocks.MessagingMock
 	kaiSdk                sdk.KaiSDK
 }
@@ -27,19 +27,23 @@ func TestMainSuite(t *testing.T) {
 
 func (s *MainSuite) SetupSuite() {
 	s.messagingMock = sdkMocks.NewMessagingMock(s.T())
+	s.metadataMock = sdkMocks.NewMetadataMock(s.T())
 	s.centralizedConfigMock = sdkMocks.NewCentralizedConfigMock(s.T())
 
 	s.kaiSdk = sdk.KaiSDK{
 		Logger:            testr.New(s.T()),
 		Messaging:         s.messagingMock,
+		Metadata:          s.metadataMock,
 		CentralizedConfig: s.centralizedConfigMock,
 	}
 }
 
 func (s *MainSuite) TearDownTest() {
 	s.messagingMock.AssertExpectations(s.T())
+	s.metadataMock.AssertExpectations(s.T())
 	s.centralizedConfigMock.AssertExpectations(s.T())
 	s.messagingMock.ExpectedCalls = nil
+	s.metadataMock.ExpectedCalls = nil
 	s.centralizedConfigMock.ExpectedCalls = nil
 	s.messagingMock.Calls = nil
 }
@@ -53,10 +57,14 @@ func (s *MainSuite) TestProcessRunnerFunc() {
 	s.centralizedConfigMock.On("GetConfig", "version").Return("version", nil)
 	s.centralizedConfigMock.On("GetConfig", "workflow").Return("workflow", nil)
 	s.centralizedConfigMock.On("GetConfig", "process").Return("process", nil)
-	s.centralizedConfigMock.On("GetConfig", "retainExecutionId", messaging.ProcessScope).Return("true", nil)
+	s.metadataMock.On("GetProduct").Return("productID", nil)
+	s.metadataMock.On("GetVersion").Return("versionID", nil)
+	s.metadataMock.On("GetWorkflow").Return("workflowID", nil)
+	s.metadataMock.On("GetProcess").Return("processID", nil)
+	s.centralizedConfigMock.On("GetConfig", "retain-execution-id").Return("true", nil)
 	s.messagingMock.On("SendOutputWithRequestID", mock.Anything, mock.Anything).Return(nil)
 
 	go func() {
-		processRunner(nil, s.kaiSdk)
+		processSubscriberRunner(nil, s.kaiSdk)
 	}()
 }
